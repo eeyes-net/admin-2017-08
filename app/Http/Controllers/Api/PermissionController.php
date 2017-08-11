4ecde4734604eb8ba0b9f7323670b76618721060
Ganlv <ganlvtech@qq.com>
2017-08-11 01:04:14 +0100

    修复错误、改进API

--- app/Http/Controllers/Api/PermissionController.php
+++ app/Http/Controllers/Api/PermissionController.php
@@ -6,19 +6,28 @@ use App\Http\Controllers\Controller;
 use Encore\Admin\Auth\Database\Administrator;
 use Illuminate\Http\Request;
 
-class AdminController extends Controller
+class PermissionController extends Controller
 {
     public function can($username, Request $request)
     {
         $permission = $request->get('permission');
-        $admin = Administrator::where('username', $username)->get();
+        $admin = Administrator::where('username', $username)->first();
+        $can = null;
+        $msg = 'Unknown error';
         if ($admin) {
             $can = $admin->can($permission);
+            if ($can) {
+                $msg = 'OK';
+            } else {
+                $msg = 'Forbidden';
+            }
         } else {
+            $msg = 'User not exists';
             $can = false;
         }
         return [
             'can' => $can,
+            'msg' => $msg,
         ];
     }
 }
